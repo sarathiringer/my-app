df0fee6d412df89fbfc45c69d48384c7f6dcd14d
Sara Thiringer <s.thiringer@gmail.com>
2020-03-29 16:09:03 +0200

    Folder for functions in processing

--- server/upload.js
+++ server/upload.js
@@ -1,6 +1,9 @@
 
 const IncomingForm = require('formidable').IncomingForm
 const fs = require('fs');
+const { cleaningText, findTopWord, getProcessedText } = require('./utils/processing.js')
+
+
 module.exports = function upload(req, res) {
 
 const form = new IncomingForm()
@@ -13,71 +16,27 @@ form.on('file', (field, file) => {
     // Split into array for later use
     original_text_array = data.split(" ");
 
-    // Remove dots, commas and newline + maks small letters
-    data = data.replace(",", "");
-    data = data.replace(".", "");
-    data = data.replace("\\n", "");
-
-    for (i = 0; i < 20; i++){
-        data = data.replace("\\n".concat(i));
-    };
-
-    data = data.toLowerCase();
+    // Remove dots etc
+    data = cleaningText(data);
     
     // Split text into array
     let array_words = data.split(" ");
 
     // Find the most frequent word
-    let top_word;
-    let comparison = -1;
-    let count = {};
-
-    for (let i = 0; i < array_words.length; i++){
-        
-        if (count[array_words[i]] === undefined){
-            count[array_words[i]] = 0;
-        } else {
-            count[array_words[i]]++;
-        };
-
-        if (count[array_words[i]] > comparison){
-            comparison = count[array_words[i]];
-            top_word = array_words[i]
-        };
-    };
+    top_word = findTopWord(array_words);
 
     // Wrap the most frequent word in 'foo' and 'bar'
     foobar = 'foo'.concat(top_word, 'bar');
     console.log(foobar);
 
     // Put the new word into the old array
-    let top_word_capital = top_word.charAt(0).toUpperCase();
-
-    for (let i = 0; i < original_text_array.length; i++){
-        if (original_text_array[i] === top_word){
-            original_text_array[i] = foobar;
-        } else if (original_text_array[i] === top_word.concat(",")){
-            original_text_array[i] = foobar.concat(",");
-        } else if (original_text_array[i] === top_word.concate(".")){
-            original_text_array[i] = foobar.concat(".");
-        } else if (original_text_array[i].includes("\\n") && original_text_array[i].includes(top_word)){
-            let nr = original_text_array[i].slice(-1);
-            original_text_array[i] = foobar.concat("\\n", nr);
-        } else if (original_text_array[i] === top_word_capital){
-            original_text_array[i] = foobar;
-        } else if (original_text_array[i] === top_word_capital.concat(",")){
-            original_text_array[i] = foobar.concat(",");
-        } else if (original_text_array[i] === top_word_capital.concate(".")){
-            original_text_array[i] = foobar.concat(".");
-        } else if (original_text_array[i].includes("\\n") && original_text_array[i].includes(top_word_capital)){
-            let nr = original_text_array[i].slice(-1);
-            original_text_array[i] = foobar.concat("\\n", nr);
-        }
-    };
+    original_text_array = getProcessedText(top_word, original_text_array)
 
     // Turn back to string
     let processed_text = original_text_array.join(" ");
-
+    
+    
+    console.log(processed_text);
 
 })
 
